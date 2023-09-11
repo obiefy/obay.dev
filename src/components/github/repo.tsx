6feb4edf246a20ba2c91e6de4d9f5ed2042747a3
Obay Adam <obayhamed@gmail.com>
2023-09-11 21:29:12 +0300

    feature: add cool animation

--- src/components/github/repo.tsx
+++ src/components/github/repo.tsx
@@ -8,10 +8,14 @@ export default function Card({repo}: { repo: Repo }) {
         <Link href={repo.url} className="text-stone-600 text-sm font-semibold hover:underline underline-lg">@{repo.name}</Link>
         <div className="flex text-stone-400 mt-[2px]">
           <div className="flex">
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
-            </svg>
+            <span className="relative flex h-4 w-4">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-stone-400 opacity-30"></span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="relative inline-flex w-4 h-4" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"></path>
+              </svg>
+            </span>
+            
             <p className="text-xs mx-1">{repo.stars}</p>
           </div>
           <div className="flex mx-1">
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,10 +7,12 @@ export default function Hero() {
     <div className="lg:flex md:flex">
       <div className="w-full lg:w-1/2 text-stone-800">
         <div className="lg:pr-4 mb-4">
-          <p className="mb-4">
-            Hey there! 👋 I&apos;m 
-            <h1 className="mt-8 mb-4 text-7xl font-extrabold font-calibre text-sky-700">Obay Adam</h1>
-          </p>
+          <div className="mb-4 flex">
+            Hey there! 
+            <div className="w-2 ml-2 mr-6 -mt-2 text-2xl animate-wave">👋</div>
+            I&apos;m
+          </div>
+          <h1 className="mt-8 mb-4 text-7xl font-extrabold font-calibre text-sky-700">Obay Adam</h1>
           <p className="leading-relaxed">
             I enjoy building web applications. My interest about coding started back in 2012, I created a very simple calculator using <Link className="font-medium link" href={'https://www.freepascal.org/'}>Pascal</Link> instructions. 
             Right after, I completed my B.S. in Software Engineering, since then my journey as a software developer has been filled with exciting challenges and rewarding experiences.
