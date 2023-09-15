6ff815f48b3bbab730768035b04d06589b03f61b
Obay Adam <obayhamed@gmail.com>
2023-09-16 00:35:40 +0300

    feature: change design to simple one page inspired by leerob.io

--- src/components/code.tsx
+++
@@ -1,7 +0,0 @@
-export default function Code({ method, className}: { method: string, className?: string }) {
-  return (
-    <div className={`text-stone-500 ${className}`}>
-        <span className="text-sky-700">obay</span>.<span className="text-sky-700">{method}</span>();
-    </div>
-  );
-}
---
+++ src/components/gallery/developer.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+import image1 from '../../../public/images/developer/1.jpg';
+import image2 from '../../../public/images/developer/2.jpg';
+import image3 from '../../../public/images/developer/3.jpeg';
+import image4 from '../../../public/images/developer/4.jpg';
+import image5 from '../../../public/images/developer/5.jpg';
+
+export default function DeveloperGallery() {
+  return (
+    <div className="mt-8 grid grid-cols-3 gap-2">
+      <Image className="image row-span-2 col-span-2 " alt='Working' src={image1}/>
+      <Image className="image row-span-2" alt='Working' src={image2}/>
+      <Image className="image" alt='Working' src={image3}/>
+      <Image className="image" alt='Working' src={image4}/>
+      <Image className="image object-left-bottom" alt='Working' src={image5}/>
+    </div>
+  );
+}
