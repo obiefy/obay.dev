9d04988b29b2615539e75e1b6f23b0d4c1db3244
Obay Adam <obayhamed@gmail.com>
2023-09-20 16:35:26 +0300

    feature: improve performance add remove unused fonts

--- src/components/gallery/developer.tsx
+++ src/components/gallery/developer.tsx
@@ -1,19 +1,59 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import image1 from '../../../public/images/developer/1.jpg';
-import image2 from '../../../public/images/developer/2.jpg';
-import image3 from '../../../public/images/developer/3.jpeg';
-import image4 from '../../../public/images/developer/4.jpg';
-import image5 from '../../../public/images/developer/5.jpg';
+import image1 from "../../../public/images/developer/1.jpg";
+import image2 from "../../../public/images/developer/2.jpg";
+import image3 from "../../../public/images/developer/3.jpeg";
+import image4 from "../../../public/images/developer/4.jpg";
+import image5 from "../../../public/images/developer/5.jpg";
 
 export default function DeveloperGallery() {
   return (
     <div className="mt-8 grid grid-cols-2 md:grid-cols-3 gap-2">
-      <Image className="image row-span-2 col-span-2" alt="Working at a library" src={image1} priority />
-      <Image className="image md:row-span-2" alt="Working from home :)" src={image2} priority />
-      <Image className="image" alt="Working from home :)" src={image3} priority />
-      <Image className="image" alt="Working from home :)" src={image4} priority />
-      <Image className="image object-left-bottom" alt="Working from home :)" src={image5} priority/>
+      <Image
+        className="image row-span-2 col-span-2"
+        alt="Working at a library"
+        src={image1}
+        quality={80}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        priority
+      />
+      <Image
+        className="image md:row-span-2"
+        alt="Working from home :)"
+        src={image2}
+        quality={50}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        priority
+      />
+      <Image
+        className="image"
+        alt="Working from home :)"
+        src={image3}
+        quality={40}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        priority
+      />
+      <Image
+        className="image"
+        alt="Working from home :)"
+        src={image4}
+        quality={40}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        priority
+      />
+      <Image
+        className="image object-left-bottom"
+        alt="Working from home :)"
+        src={image5}
+        quality={40}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        priority
+      />
     </div>
   );
 }
--- src/components/gallery/traveler.tsx
+++ src/components/gallery/traveler.tsx
@@ -1,23 +1,72 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import image1 from '../../../public/images/traveler/1.jpeg';
-import image2 from '../../../public/images/traveler/2.jpeg';
-import image3 from '../../../public/images/traveler/3.jpeg';
-import image4 from '../../../public/images/traveler/4.png';
-import image5 from '../../../public/images/traveler/5.jpg';
-import image6 from '../../../public/images/traveler/6.png';
-import image7 from '../../../public/images/traveler/7.png';
+import image1 from "../../../public/images/traveler/1.jpeg";
+import image2 from "../../../public/images/traveler/2.jpeg";
+import image3 from "../../../public/images/traveler/3.jpeg";
+import image4 from "../../../public/images/traveler/4.png";
+import image5 from "../../../public/images/traveler/5.jpg";
+import image6 from "../../../public/images/traveler/6.png";
+import image7 from "../../../public/images/traveler/7.png";
 
 export default function TravelerGallery() {
   return (
     <div className="mt-8 grid grid-cols-2 md:grid-cols-4 gap-2">
-      <Image className="col-span-2 row-span-2 image" alt="Paragliding for the first time in Oludeniz, Fethiye" src={image1}/>
-      <Image className="md:row-span-2 image" alt="Backpacking in Turkey" src={image5}/>
-      <Image className="md:row-span-2 image" alt="Skiing on the top of Uludag mountain in Bursa, Turkey" src={image2}/>
-      <Image className="image" alt="Cycling in the princes island in Istanbul" src={image3}/>
-      <Image className="image" alt="Backpacking in Fethiye" src={image4}/>
-      <Image className="image" alt="FIFA World Cup Qatar 2022, England vs Senegal" src={image6}/>
-      <Image className="image" alt="Tomb of Amyntas, Fethiye" src={image7}/> 
+      <Image
+        className="col-span-2 row-span-2 image"
+        alt="Paragliding for the first time in Oludeniz, Fethiye"
+        src={image1}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        quality={80}
+      />
+      <Image
+        className="md:row-span-2 image"
+        alt="Backpacking in Turkey"
+        src={image5}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        quality={50}
+      />
+      <Image
+        className="md:row-span-2 image"
+        alt="Skiing on the top of Uludag mountain in Bursa, Turkey"
+        src={image2}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        quality={40}
+      />
+      <Image
+        className="image"
+        alt="Cycling in the princes island in Istanbul"
+        src={image3}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        quality={40}
+      />
+      <Image
+        className="image"
+        alt="Backpacking in Fethiye"
+        src={image4}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        quality={40}
+      />
+      <Image
+        className="image"
+        alt="FIFA World Cup Qatar 2022, England vs Senegal"
+        src={image6}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        quality={40}
+      />
+      <Image
+        className="image"
+        alt="Tomb of Amyntas, Fethiye"
+        src={image7}
+        placeholder="blur"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        quality={40}
+      />
     </div>
   );
 }
